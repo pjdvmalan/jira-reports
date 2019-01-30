7259ce3655e54757392590bf9ad447dd1026148f
Pieter Malan <pmalan@2u.com>
2019-01-30 22:47:42 +0200

    Added BSA

--- jira-reports.py
+++ jira-reports.py
@@ -6,12 +6,11 @@ from jira import JIRA
 import dateutil.parser
 import datetime
 import time
-import csv
 
 from etc import config
 import lib
 
-def fetch_jira_tasks():
+def fetch_jira_tasks(maxResults, startAt):
     jira = JIRA(options=config.OPTIONS, basic_auth=config.AUTH)
 
     # Story points are a computed field and may differ between projects.
@@ -21,21 +20,24 @@ def fetch_jira_tasks():
     # Print all fields for this Jira project.
     # print (field_map)
 
-    # issues = jira.search_issues(jql_str='issue = CTCRM-52', maxResults=1,expand=["changelog", "transitions", "versionedRepresentations"])
-    issues = jira.search_issues(jql_str='project in (PTECH, CTMD, CTFIND, CTCRM, CTED) ORDER BY Rank ASC', maxResults=10000,expand=["changelog", "transitions", "versionedRepresentations"])
+    # issues = jira.search_issues(jql_str='project in (PTECH, CTMD, CTFIND, CTCRM, CTED) ORDER BY Rank ASC', maxResults=maxResults, startAt=startAt, expand=["changelog", "transitions", "versionedRepresentations"])
+    issues = jira.search_issues(jql_str='issue = CTED-170', maxResults=1,expand=["changelog", "transitions", "versionedRepresentations"])
     # issues = jira.search_issues(jql_str='project = PTECH AND issue = PTECH-103', maxResults=1,expand=["changelog","transitions"])
     # issues = jira.search_issues(jql_str='project = PTECH', maxResults=2,expand=["changelog","transitions"])
 
     # Buils a dictionary of statuses. We need to do this by cycling through all 
     # issues to ensure that we have a template to use during processing.
     statuses = {}
+    statuses_week_of_year = {}
     for issue in issues:
         changelog = issue.changelog
         for history in changelog.histories:
             for item in history.items:
                 if item.field == 'status':
                     statuses[item.fromString] = 0
+                    statuses_week_of_year["{0}_WOY".format(item.fromString)] = 0
         statuses[issue.fields.status.name] = 0
+        statuses_week_of_year["{0}_WOY".format(issue.fields.status.name)] = 0
 
     rows = []
 
@@ -60,12 +62,26 @@ def fetch_jira_tasks():
 
             assigned_to=issue.fields.assignee.name if issue.fields.assignee else '',
             story_points=story_points if story_points else 0,
-            developer = ''
+            bsa = '',
+            developer = '',
+            developer_assigned_date = '',
+            sprint_state = '',
+            sprint_name = '',
+            sprint_start_date = '',
+            sprint_end_date = ''
         )
+
+        sprint = lib.sprint_str_to_dict(getattr(issue.fields, field_map['Sprint']))
+        if sprint:
+            row['sprint_name'] = sprint['name']
+            row['sprint_start_date'] = sprint['startDate']
+            row['sprint_end_date'] = sprint['endDate']
+            row['sprint_state'] = sprint['state']
 
         # Add statuses to the current dictionary.
         row.update(statuses)
-        
+        row.update(statuses_week_of_year)
+
         start_date = None
 
         # Determine days spend in specific status.
@@ -81,23 +97,36 @@ def fetch_jira_tasks():
                             start_date = dateutil.parser.parse(issue.fields.created)
                         
                         end_date = dateutil.parser.parse(history.created)
-                        days = lib.date_diff_weekdays_only(start_date, end_date)
-                        start_date = end_date
-                        row[item.fromString] = row[item.fromString] + days
 
-                        row['status_changed_on']=dateutil.parser.parse(history.created).strftime("%Y-%m-%d %H:%M:%S")
-                        row['status_changed_week_of_year']=int(dateutil.parser.parse(history.created).strftime("%W"))
+                        row[item.fromString] = row[item.fromString] + lib.business_hours(start_date, end_date)
+                        row["{0}_WOY".format(item.fromString)] = int(dateutil.parser.parse(history.created).strftime("%W"))
+
+                        start_date = end_date
+
+                        row['status_changed_on'] = dateutil.parser.parse(history.created).strftime("%Y-%m-%d %H:%M:%S")
+                        row['status_changed_week_of_year'] = int(dateutil.parser.parse(history.created).strftime("%W"))
 
                     # Check if it is a developer.
                     if item.field == 'assignee':
-                        if item.fromString in config.DEVELOPERS:
-                            row['developer'] = item.fromString
+                        if item.toString in config.DEVELOPERS:
+                            row['developer'] = item.toString
+                            row['developer_assigned_date'] = dateutil.parser.parse(history.created).strftime("%Y-%m-%d %H:%M:%S")
+                        if item.toString in config.BSAS:
+                            row['bsa'] = item.toString
 
+        # Calculate hours for status that has not changed yet.
+        if row[issue.fields.status.name] == 0:
+            row[issue.fields.status.name] = lib.business_hours(dateutil.parser.parse(issue.fields.created))
+            row["{0}_WOY".format(issue.fields.status.name)] = int(dateutil.parser.parse(issue.fields.created).strftime("%W"))
 
-        else:
-            # No history yet, so take the date created.
-            days = lib.date_diff_weekdays_only(dateutil.parser.parse(issue.fields.created))
-            row[issue.fields.status.name] = days
+        # If there is no history for assignee change, we need to get it from the default.
+        if issue.fields.assignee:
+            if row['developer'] == '' and issue.fields.assignee.displayName in config.DEVELOPERS:
+                row['developer'] = issue.fields.assignee.displayName
+                row['developer_assigned_date'] = dateutil.parser.parse(issue.fields.created).strftime("%Y-%m-%d %H:%M:%S")
+
+            if row['bsa'] == '' and issue.fields.assignee.displayName in config.BSAS:
+                row['bsa'] = issue.fields.assignee.displayName
 
         rows.append(row)
 
@@ -106,20 +135,14 @@ def fetch_jira_tasks():
 
     return rows
 
-def output_to_csv(rows, output_path, header=None):
-
-    with open(output_path, 'w') as f_out:
-        writer = csv.DictWriter(f_out, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(rows)
-
 def main():
-    
-    rows = fetch_jira_tasks()
-    
-    if rows:
-        headers = rows[0].keys()
-        output_to_csv(rows, config.REPORT_CSV_PATH, headers)
+
+    maxResults = 900
+    startAt = 0
+
+    rows = fetch_jira_tasks(maxResults, startAt)
+    headers = rows[0].keys()
+    lib.output_to_csv(rows, config.REPORT_CSV_PATH, headers)
 
 if __name__ == '__main__':
     main()
--- lib/__init__.py
+++ lib/__init__.py
@@ -4,21 +4,42 @@ Initliazation file for library module.
 
 # import datetime
 from datetime import datetime, timezone
-import pytz
+import businesstime
+import csv
 
-def date_diff_weekdays_only(start_date, end_date=None):
-    """
-    Calculate the number of work days between two dates.
-
-    Taken from: https://stackoverflow.com/questions/46386764/python-exclude-weekends-between-two-dates
-
-    @param start_date: The start date.
-    @param end_date: The end date.
-    @return: Number of week days.
-    """
+def business_hours(start_date, end_date=None):
     if not end_date:
-        end_date = datetime.now(pytz.timezone('US/Eastern'))
+        end_date = datetime.now()
 
-    delta = (end_date - start_date).days
+    start_d = start_date.replace(tzinfo=None)
+    end_d = end_date.replace(tzinfo=None)
 
-    return (delta - (delta // 7) * 2)
+    bt = businesstime.BusinessTime()
+    hours = bt.businesstime_hours(start_d, end_d).seconds/60/60
+
+    return hours
+
+
+def sprint_str_to_dict(f):
+    # Transform the following format into a dictionary: 'com.atlassian.greenhopper.service.sprint.Sprint@6316c6c0[id=1387,rapidViewId=454,state=CLOSED,name=Week 30-31,startDate=2018-07-23T12:42:06.058Z,endDate=2018-08-06T12:42:00.000Z,completeDate=2018-08-06T07:57:14.389Z,sequence=1387,goal=]'
+    if f:
+        try:
+            if isinstance(f, list):
+                f = f[0]
+            fIdx = f.index('[')
+            lIdx = f.index(']')
+            if fIdx > 0 and lIdx > 0:
+                f = f[fIdx+1:lIdx]
+                pairs = f.split(',')
+                return dict([p.split('=') for p in pairs])
+        except Exception as e:
+            print("Type error: " + str(e))
+
+    return None
+
+def output_to_csv(rows, output_path, header=None):
+
+    with open(output_path, 'w') as f_out:
+        writer = csv.DictWriter(f_out, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(rows)
